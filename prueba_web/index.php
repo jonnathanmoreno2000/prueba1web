42f188e99a29785901639e41d43f6c937b9d2df3
jonnathanmoreno2000 <65380631+jonnathanmoreno2000@users.noreply.github.com>
2020-05-17 23:31:51 -0400

    PruebaWEb

---
+++ prueba_web/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilo/estilo.css">
+    <title>Esta es mi web</title>
+</head>
+
+<body>
+    <div id="contenedor">
+        <header>
+            <nav><h1>HOLA MUNDO</h1></nav>
+        </header>
+        <section>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus blanditiis nostrum reiciendis facere harum! Aspernatur obcaecati vitae numquam nostrum molestias.</p>
+
+            <aside>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ab quibusdam illum sint similique architecto non distinctio facilis modi incidunt! Eum sapiente, in sunt id facilis, voluptate eaque, totam nobis dicta et eveniet quaerat! Error vel laudantium ducimus, molestiae soluta eos commodi eum repellat consequuntur magnam vero optio. Provident eveniet error iste. Quidem, aliquam accusamus? Maxime deleniti illum velit dolores itaque tempora, omnis sequi, maiores unde, quia praesentium ducimus temporibus. Sed maxime pariatur officia, nemo itaque dolore aut recusandae, perferendis, quas harum libero numquam. Asperiores similique, molestiae rem maxime eaque ducimus, quo assumenda distinctio aspernatur nisi expedita explicabo aperiam tenetur in mollitia, impedit reiciendis. Excepturi quam minima explicabo, corporis magni velit placeat natus, accusantium in voluptatum reiciendis dolorem sit necessitatibus dicta voluptates aspernatur tempore quasi harum aliquid libero nam. Iure aspernatur perferendis ea a quisquam saepe maiores voluptatem voluptates tenetur ex cumque sint nisi, perspiciatis fuga, magnam error illo minima?</p>
+            </aside>
+        </section>
+        <footer><h2>HOLA A TODOS</h2></footer>
+        <img src="https://cdn.themedizine.com/2018/05/64248893-DEB4-41E8-960A-B335BB1B7FCA.jpeg" />
+        <h3>Jonnathan Moreno</h3>
+        <h4>+56999999999</h4>
+        <h5>afskskjdgfjdh@gmail.com</h5>
+    </div>
+</body>
+
+</html>
